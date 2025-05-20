51c80a55bcc75f7d3905e130481501a274aa85a7
Ezinwa Victor <Ezinwa.Victor@hwdevelopment.eu>
2025-05-20 17:06:58 +0100

    add bottom paddings

--- app/src/main/java/com/vectorinc/moniepointchallenge/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/vectorinc/moniepointchallenge/ui/BottomNavigationBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -51,6 +52,7 @@ fun BottomNavigationBar(navController: NavController) {
         shadowElevation = 8.dp,
         modifier = Modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
             .height(84.dp),
         color = Color.White
     ) {
